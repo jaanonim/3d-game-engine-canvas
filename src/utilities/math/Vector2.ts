52b5a8109d15d036c01dd767dcb6ef14ec1d01db
Jaanonim <mat8mro@gmail.com>
2022-10-07 12:30:28 +0200

    update Vector2

--- src/utilities/math/Vector2.ts
+++ src/utilities/math/Vector2.ts
@@ -4,6 +4,9 @@ export default class Vector2 {
 
     static zero: Vector2 = new Vector2(0, 0);
     static up: Vector2 = new Vector2(0, 1);
+    static down: Vector2 = new Vector2(0, -1);
+    static left: Vector2 = new Vector2(-1, 0);
+    static right: Vector2 = new Vector2(1, 0);
     static one: Vector2 = new Vector2(1, 1);
 
     constructor(x: number, y: number) {
