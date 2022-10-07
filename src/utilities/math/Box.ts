8b9fa265f5a538d4fd980dcfa484222cd396bf33
Jaanonim <mat8mro@gmail.com>
2022-10-07 15:47:44 +0200

    add Boxes

---
+++ src/utilities/math/Box.ts
@@ -0,0 +1,32 @@
+import { clamp } from "./Math";
+import Vector3 from "./Vector3";
+
+export default class Box {
+    a: Vector3;
+    b: Vector3;
+
+    constructor(a: Vector3, b: Vector3) {
+        this.a = a;
+        this.b = b;
+    }
+
+    clamp(v: Vector3) {
+        return new Vector3(
+            clamp(
+                v.x,
+                Math.min(this.a.x, this.b.x),
+                Math.max(this.a.x, this.b.x)
+            ),
+            clamp(
+                v.y,
+                Math.min(this.a.y, this.b.y),
+                Math.max(this.a.y, this.b.y)
+            ),
+            clamp(
+                v.z,
+                Math.min(this.a.z, this.b.z),
+                Math.max(this.a.z, this.b.z)
+            )
+        );
+    }
+}
---
+++ src/utilities/math/Box2D.ts
@@ -0,0 +1,27 @@
+import { clamp } from "./Math";
+import Vector2 from "./Vector2";
+
+export default class Box2D {
+    a: Vector2;
+    b: Vector2;
+
+    constructor(a: Vector2, b: Vector2) {
+        this.a = a;
+        this.b = b;
+    }
+
+    clamp(v: Vector2) {
+        return new Vector2(
+            clamp(
+                v.x,
+                Math.min(this.a.x, this.b.x),
+                Math.max(this.a.x, this.b.x)
+            ),
+            clamp(
+                v.y,
+                Math.min(this.a.y, this.b.y),
+                Math.max(this.a.y, this.b.y)
+            )
+        );
+    }
+}
